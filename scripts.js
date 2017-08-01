8a52708c23f4ee4045936192d85bc59213d5cf7f
monikasmilga <tuuli705@gmail.com>
2017-08-01 10:10:08 +0300

    Initializing function, game states

--- scripts.js
+++ scripts.js
@@ -4,3 +4,35 @@
 $("h1").click(function () {
     $("h1").hide();
 })
+
+var FastTyping = function () {
+
+    const STATE_REGISTER = 'registration state',
+        STATE_LEVEL = 'level selection state',
+        STATE_GAME = 'game state',
+        STATE_END = 'game over state';
+
+    function changeState(value) {
+
+        switch (value) {
+            case STATE_REGISTER:
+                break;
+            case STATE_LEVEL:
+                break;
+            case STATE_GAME:
+                break;
+            case STATE_END:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    function initialize() {
+        console.log('working');
+
+
+    }
+
+};
